8c82287c3ae0fa299af4a56b9eeec1ff6d57fcbb
Rafael Maximo <rafael.ribeiro@faculdadeimpacta.com.br>
2024-05-02 20:24:04 -0300

    adiciona campos do form e tratamento inicial JS

--- cep.js
+++ cep.js
@@ -1,12 +1,41 @@
 const cepInput = document.getElementById("cep");
 if (cepInput !== null) cepInput.addEventListener("keyup", handleCepChange);
 
-function handleCepChange(event) {
-  const target = event.target;
-  valor = target.value;
-  console.log(valor);
-  if (valor.length === 8) {
-    console.log("terminou de digitar o cep");
-    target.readOnly = true;
+async function getCepData(cep) {
+  try {
+    response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    console.log("resposta do fetch", response);
+    data = await response.json();
+    console.log("dados da resposta", data);
+    return data;
+  } catch (erro) {
+    console.log("trantando o erro", erro);
+    return null;
   }
+}
+
+function fillInAddress(data) {
+  console.log("preenchendo o endereço", data);
+}
+
+async function handleCepChange(event) {
+  const target = event.target;
+  value = target.value;
+  console.log(value);
+
+  if (value.length !== 8) return;
+
+  const cepData = await getCepData(value);
+
+  if (cepData === null) {
+    alert("Digite um valor válido de CEP!");
+    return;
+  }
+
+  if ("erro" in cepData) {
+    alert("CEP não encontrado, digite o endereço manualmente.");
+    return;
+  }
+
+  fillInAddress(cepData);
 }
