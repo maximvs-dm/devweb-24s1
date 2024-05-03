6c9b43e8048cf643ff7b9534dadf26f690723198
Rafael Maximo <rafael.ribeiro@faculdadeimpacta.com.br>
2024-05-02 21:40:59 -0300

    adiciona limpeza dos campos ao deletar o cep

--- cep.js
+++ cep.js
@@ -1,5 +1,15 @@
 const cepInput = document.getElementById("cep");
-if (cepInput !== null) cepInput.addEventListener("keyup", handleCepChange);
+if (cepInput !== null) {
+  cepInput.addEventListener("keyup", handleCepChange);
+  cepInput.addEventListener("keydown", handleCepDelete);
+}
+
+const fieldIds = {
+  rua: "rua",
+  bairro: "bairro",
+  cidade: "cidade",
+  estado: "estado",
+};
 
 async function getCepData(cep) {
   try {
@@ -14,14 +24,25 @@ async function getCepData(cep) {
   }
 }
 
+function fillInAddressField(id, value, readonly = true) {
+  element = document.getElementById(id);
+  element.value = value;
+  element.readOnly = readonly;
+}
+
 function fillInAddress(data) {
-  console.log("preenchendo o endereço", data);
+  fillInAddressField(fieldIds.rua, data.logradouro);
+  fillInAddressField(fieldIds.bairro, data.bairro);
+  fillInAddressField(fieldIds.cidade, data.localidade);
+  fillInAddressField(fieldIds.estado, data.uf);
 }
 
 async function handleCepChange(event) {
+  const keyCode = event.keyCode;
+  if (keyCode < 96 || keyCode > 105) return;
+
   const target = event.target;
   value = target.value;
-  console.log(value);
 
   if (value.length !== 8) return;
 
@@ -38,4 +59,17 @@ async function handleCepChange(event) {
   }
 
   fillInAddress(cepData);
+}
+function clearAddressFields() {
+  fillInAddressField(fieldIds.rua, "", false);
+  fillInAddressField(fieldIds.bairro, "", false);
+  fillInAddressField(fieldIds.cidade, "", false);
+  fillInAddressField(fieldIds.estado, "", false);
+}
+
+function handleCepDelete(event) {
+  console.log({ code: event.keyCode, key: event.key });
+  if ([8, 46].includes(event.keyCode)) {
+    clearAddressFields();
+  }
 }
